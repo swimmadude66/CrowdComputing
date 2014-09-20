4a8b17760b73dfe7e86e64bd7667b7acf79517ee
Adam Yost <swimmadude66@gmail.com>
2014-09-20 19:32:37 -0400

    Added UDP listener on port 27693

--- Client/add_node.py
+++ Client/add_node.py
@@ -11,20 +11,17 @@ def authenticate():
     data["machine-id"] = open("/etc/machine-id", 'r').read().strip()
     data["mac"] = str(get_mac())
     # validate identify device and attempt to add
-    print "Log in to Crowd Computing to add this device"
+    print "\nLog in to Crowd Computing to add this device"
     validated = False
     while not validated:
-        data["username"] = str(raw_input('Username: '))
-        data["password"] = getpass.getpass("Password: ")
+        data["userName"] = str(raw_input("Username: "))
+        data["thePassword"] = getpass.getpass("Password: ")
         print "Attempting to add device"
-        response = requests.post("http://54.86.187.108:3000/sendNodeStatusUpdate", data)
+        response = requests.post("http://54.86.187.108:3000/AddNode", data)
         if (response.status_code == 200) and ("accepted" in response.text):
             validated = True
         else:
             print("Login failed, please try again.")
-
-
-
 
 
 if __name__ == "__main__":
---
+++ Client/status_update.py
@@ -0,0 +1,19 @@
+__author__ = 'ayost'
+
+
+import psutil
+import requests
+
+
+def check_usage():
+    status = dict()
+    status["cpucount"] = int(psutil.cpu_count())
+    status["cpuusage"] = int(psutil.cpu_percent(interval=1))
+    status["memufree"] = int(psutil.virtual_memory().free)
+    status["swapfree"] = int(psutil.swap_memory().free)
+    status["netusage"] = str(psutil.network_io_counters())
+    response = requests.post("http://54.86.187.108:3000/sendNodeStatusUpdate", status)
+
+
+if __name__ == "__main__":
+    check_usage()
