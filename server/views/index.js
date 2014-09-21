e5f6ccf191029f24186f1a60c36e9979d69fe101
George Stipick <g.stipick@att.net>
2014-09-20 21:51:23 -0400

    Made relative pathing for login for better development

--- server/views/index.js
+++ server/views/index.js
@@ -24,11 +24,11 @@ function signup() {
     verifyPasswordField.setAttribute("placeholder", "Verify Password");
     verifyPasswordField.setAttribute("required", null);
     form.insertBefore(verifyPasswordField, formElements[2]);
-    
+     
     
     formElements[3].removeEventListener("onclick", formElements[3].onclick);
     formElements[3].onclick = null;
-    formElements[3].setAttribute("formaction", "http://54.86.187.108:3000/register");
+    formElements[3].setAttribute("formaction", "/register");
     formElements[3].setAttribute("formmethod", "post");
     formElements[3].setAttribute("onsubmit", "return submitRegistration()");
     
